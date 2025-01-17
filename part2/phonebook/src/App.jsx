f2ab501b442329ad81232627cdb3f519b8641311
MarkFantini <marcelo.asfantini@gmail.com>
2025-01-16 23:59:23 -0300

    Exercise 2.1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Person = ({ name }) => {
+  return (
+    <div>{name}</div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const object = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(object))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <div>debug: {newName}</div>
+      
+      <h2>Numbers</h2>
+        {persons.map(person => 
+          <Person key={person.name} name={person.name} />
+        )}
+    </div>
+  )
+}
+
+export default App
