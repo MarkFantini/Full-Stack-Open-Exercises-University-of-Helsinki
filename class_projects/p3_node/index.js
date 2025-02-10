585a9c414a4596c0e879dc2e2ea91890081c839a
MarkFantini <marcelo.asfantini@gmail.com>
2025-02-10 19:00:44 -0300

    Added PUT route with method findByIdAndUpdate

--- class_projects/p3_node/index.js
+++ class_projects/p3_node/index.js
@@ -63,13 +63,7 @@ app.get('/api/notes/:id', (request, response, next) => {
       response.status(404).end()
     }
   })
-  // .catch(error => {
-  //   console.log(error)
-  //   response.status(400).send({
-  //     error: 'malformatted id'
-  //   })
   .catch(error => next(error))
-  })
 })
 
 
@@ -98,10 +92,32 @@ app.post('/api/notes', (request, response) => {
 
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  // const id = request.params.id
+  // notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
+  // response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 
