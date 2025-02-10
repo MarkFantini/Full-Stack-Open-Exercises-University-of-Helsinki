c9bf7fc3585858ce416ed3d59447febe622588e9
MarkFantini <marcelo.asfantini@gmail.com>
2025-02-10 18:41:54 -0300

    Created errorHandler middleware at the end of index.js

--- class_projects/p3_node/index.js
+++ class_projects/p3_node/index.js
@@ -5,8 +5,8 @@ const app = express()
 const Note = require('./models/note')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 
 const requestLogger = (request, response, next) => {
@@ -54,7 +54,7 @@ app.get('/api/notes', (request, response) => {
 
 
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
   .then(note => {
     if (note) {
@@ -63,10 +63,12 @@ app.get('/api/notes/:id', (request, response) => {
       response.status(404).end()
     }
   })
-  .catch(error => {
-    response.status(400).send({
-      error: 'malformatted id'
-    })
+  // .catch(error => {
+  //   console.log(error)
+  //   response.status(400).send({
+  //     error: 'malformatted id'
+  //   })
+  .catch(error => next(error))
   })
 })
 
@@ -111,6 +113,18 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
